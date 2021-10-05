670a7fcd77fc00c26e31e4d170edeb41084fb093
daduckster <vlasak.o@yandex.ru>
2021-10-05 20:24:17 +0200

    everything is shit

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const addNotes = document.querySelector('.add-notes');
 const notesList = document.querySelector('.notes');
 const notes = JSON.parse(localStorage.getItem('notes')) || [];
 const inputNote = document.getElementById('input-note');
+// const deleteNote = document.querySelector('.delete-note');
+
 const inputTag = document.getElementById('input-tags');
+const tags = JSON.parse(localStorage.getItem('tags')) || [];
+
+const inputFilter = document.getElementById('input-filter');
+const filterNotes = document.querySelector('.filter-notes');
 
 function addNote(e) {
 	e.preventDefault();
@@ -11,6 +17,9 @@ function addNote(e) {
 		const tagText = inputTag.value;
 		let note = { text, tagText };
 		notes.push(note);
+		if (!tags.includes(tagText)) {
+			tags.push(tagText);
+		}
 	} else {
 		const text = inputNote.value;
 		const tagText = '';
@@ -20,6 +29,7 @@ function addNote(e) {
 
 	populateList(notes, notesList);
 	localStorage.setItem('notes', JSON.stringify(notes));
+	localStorage.setItem('tags', JSON.stringify(tags));
 	inputTag.value = '';
 	this.reset();
 }
@@ -28,12 +38,45 @@ function populateList(plates = [], platesList) {
 	platesList.innerHTML = plates
 		.map(plate => {
 			return `
-            <p id="note-text">${plate.text}</p>
-            <p id="tag-text">${plate.tagText}</p>
+            <li class="note-text">${plate.text}</li>
+            <li class="tag-text">${plate.tagText}</li>
+						<button class="delete-note">X</button>
             `;
 		})
 		.join('');
 }
 
+function tagSearch() {
+	console.log(tags);
+	const searchInput = inputFilter.value;
+	const searchedTags = tags.filter(tag => {
+		return tag.includes(searchInput);
+	});
+	console.log(searchedTags);
+	const wantedNotes = notes.filter(note => {
+		for (i = 0; i < searchedTags.length; i++) {
+			if (note.tagText === searchedTags[i]) {
+				return note;
+			}
+		}
+	});
+	notesList.innerHTML = '';
+	notesList.innerHTML = wantedNotes
+		.map(note => {
+			return `
+        <li class="note-text">${note.text}</li>
+        <li class="tag-text">${note.tagText}</li>
+        `;
+		})
+		.join('');
+	console.log(wantedNotes);
+}
+
+function noteDeleting(e) {
+	console.dir(e);
+}
+
 addNotes.addEventListener('submit', addNote);
+filterNotes.addEventListener('keyup', tagSearch);
+// deleteNote.addEventListener('click', noteDeleting);
 populateList(notes, notesList);
